3fd3fb9b218ead30e17af202944ae0960416d4ba
PriyatharsanD <priyatharsan09990@gmail.com>
2025-02-06 17:35:11 +0530

    PDF Reader is added

--- BasicTestNG/src/test/java/com/testng/batchExecution/OpenAmazonTest.java
+++ BasicTestNG/src/test/java/com/testng/batchExecution/OpenAmazonTest.java
@@ -1,7 +1,14 @@
 package com.testng.batchExecution;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
 import java.time.Duration;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -15,6 +22,28 @@ public class OpenAmazonTest {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.amazon.in/");
 		driver.close();
+	}
+	@Test
+	public void readFile() throws IOException
+	{
+		File file = new File("./files/Resume-Priyatharsan D.pdf");
+		PDDocument pdf = PDDocument.load(file);
+		PDFTextStripper stripper = new PDFTextStripper();
+		String text = stripper.getText(pdf);
+		//System.out.println(text);
+		
+		
+		BufferedReader reader = new BufferedReader(new StringReader(text));
+		String lines = reader.readLine();
+		while(lines!=null)
+		{
+			System.out.println(lines);
+			lines=reader.readLine();
+		}
+		
+		PrintWriter writer = new PrintWriter(new File("./files/resume.txt"));
+		writer.write(text);
+		writer.flush();
 	}
 
 }
